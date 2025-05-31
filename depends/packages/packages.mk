97c6e917f3fb6205310043c91770fb9e7d652ebf
AlefLM <aleflm@proton.me>
2025-06-01 00:18:45 +0330

    Remove miniupnpc package and dependency from CMake and packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -13,8 +13,6 @@ sqlite_packages=sqlite
 
 zmq_packages=zeromq
 
-upnp_packages=miniupnpc
-
 darwin_native_packages = native_ds_store native_mac_alias
 
 $(host_arch)_$(host_os)_native_packages += native_b2
