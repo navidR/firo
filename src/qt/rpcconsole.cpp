1f9b8064c5599baeac03d88c0d2936a51db6ca28
AlefLm <AlefLm@proton.me>
2025-07-22 02:18:01 +0330

    Refactor: replace copy constructor with constructor (copy constructor is protected in Qt6)

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -496,7 +496,14 @@ bool RPCConsole::eventFilter(QObject* obj, QEvent *event)
         case Qt::Key_PageDown:
             if(obj == ui->lineEdit)
             {
-                QApplication::postEvent(ui->messagesWidget, new QKeyEvent(*keyevt));
+                QApplication::postEvent(ui->messagesWidget, new QKeyEvent(
+                    keyevt->type(),
+                    keyevt->key(),
+                    keyevt->modifiers(),
+                    keyevt->text(),
+                    keyevt->isAutoRepeat(),
+                    keyevt->count()
+                ));
                 return true;
             }
             break;
@@ -504,7 +511,14 @@ bool RPCConsole::eventFilter(QObject* obj, QEvent *event)
         case Qt::Key_Enter:
             // forward these events to lineEdit
             if(obj == autoCompleter->popup()) {
-                QApplication::postEvent(ui->lineEdit, new QKeyEvent(*keyevt));
+                QApplication::postEvent(ui->lineEdit, new QKeyEvent(
+                    keyevt->type(),
+                    keyevt->key(),
+                    keyevt->modifiers(),
+                    keyevt->text(),
+                    keyevt->isAutoRepeat(),
+                    keyevt->count()
+                ));
                 return true;
             }
             break;
@@ -517,7 +531,14 @@ bool RPCConsole::eventFilter(QObject* obj, QEvent *event)
                   ((mod & Qt::ShiftModifier) && key == Qt::Key_Insert)))
             {
                 ui->lineEdit->setFocus();
-                QApplication::postEvent(ui->lineEdit, new QKeyEvent(*keyevt));
+                QApplication::postEvent(ui->lineEdit, new QKeyEvent(
+                    keyevt->type(),
+                    keyevt->key(),
+                    keyevt->modifiers(),
+                    keyevt->text(),
+                    keyevt->isAutoRepeat(),
+                    keyevt->count()
+                ));
                 return true;
             }
         }
